852a03b8bb90812cafdb9266e30a515f0a450e34
Serhii Didenko <serhii.didenko@icloud.com>
2025-12-12 03:35:45 +0200

    fix: 1.1, 1.2

--- part 1/task 1.1/course-Information1/src/components/Content.jsx
+++ part 1/task 1.1/course-Information1/src/components/Content.jsx
@@ -1,6 +1,10 @@
-const Content = ({part, exercise}) => {
+const Content = ({ part1, exercise1, part2, exercise2, part3, exercise3 }) => {
   return (
-    <p>{part} {exercise}</p>
+    <div>
+      <p>{part1} {exercise1}</p>
+      <p>{part2} {exercise2}</p>
+      <p>{part3} {exercise3}</p>
+    </div>
   )
 }
 
