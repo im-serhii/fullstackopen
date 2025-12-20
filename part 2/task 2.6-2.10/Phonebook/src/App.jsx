03ec1cb4cab111bfb01df0f1250345a892c7bf40
Serhii Didenko <serhii.didenko@icloud.com>
2025-12-20 19:04:59 +0200

    chore: 2.9*: The Phonebook Step 4

--- part 2/task 2.6-2.10/Phonebook/src/App.jsx
+++ part 2/task 2.6-2.10/Phonebook/src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '+35840544544356' },
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
 	])
+
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
+
+	const [search, setSearch] = useState('')
 
 	const addNewNumberHandler = e => {
 		e.preventDefault()
 		if (!persons.some(person => person.name === newName)) {
-			setPersons(persons.concat({ name: newName, number: newNumber }))
+			setPersons(
+				persons.concat({
+					name: newName,
+					number: newNumber,
+					id: persons[persons.length - 1].id + 1,
+				})
+			)
 			setNewName('')
 			setNewNumber('')
 			return
@@ -20,12 +32,26 @@ const App = () => {
 		setNewNumber('')
 	}
 
+	const personsToShow = persons.filter(person =>
+		person.name.toLowerCase().includes(search.toLowerCase())
+	)
+
 	const inputNewNameHandler = e => setNewName(e.target.value)
 	const inputNewNumberHandler = e => setNewNumber(e.target.value)
+	const searchHandler = e => setSearch(e.target.value)
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with{' '}
+				<input
+					onChange={searchHandler}
+					value={search}
+					type='text'
+				/>
+			</div>
+			<h1>add a new</h1>
 			<form onSubmit={addNewNumberHandler}>
 				<div>
 					name:{' '}
@@ -46,8 +72,8 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map(person => (
-				<p key={person.name}>
+			{personsToShow.map(person => (
+				<p key={person.id}>
 					{person.name} {person.number}
 				</p>
 			))}
