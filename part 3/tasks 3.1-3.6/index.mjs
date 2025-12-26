4f177a1be1f9675dd4771153ea98340c43369125
Serhii Didenko <serhii.didenko@icloud.com>
2025-12-26 13:35:22 +0200

    chore: 3.5: Phonebook backend step 5

--- part 3/tasks 3.1-3.6/index.mjs
+++ part 3/tasks 3.1-3.6/index.mjs
@@ -27,6 +27,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/persons', (req, res) => {
 	res.json(persons);
 })
@@ -55,6 +57,21 @@ app.delete('/api/persons/:id', (req, res) => {
 	persons = persons.filter(p => p.id !== id);
 
 	res.status(204).end();
+})
+
+app.post('/api/persons', (req, res) => {
+	const person = req.body;
+	const id = Math.floor(Math.random() * 1000000);
+
+	const newPerson = {
+		id: String(id + 1),
+		name: person.name,
+		number: person.number,
+	}
+
+	persons = persons.concat(newPerson)
+
+	res.status(201).json(newPerson).end();
 })
 
 app.listen(PORT);
