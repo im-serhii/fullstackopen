e3c2ac916db22164337f9483c0c4e83bd42cbb21
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-03 01:21:48 +0200

    chore: task 4.5*: Helper Functions and Unit Tests, step 3

--- part 4/tasks-4.1-4.2/utils/blogListHelper.mjs
+++ part 4/tasks-4.1-4.2/utils/blogListHelper.mjs
@@ -5,3 +5,15 @@ export const dummy = (bloglist) => {
 export const totalLikes = (bloglist) => {
 	return bloglist.reduce((likes, blog) => likes + blog.likes, 0)
 }
+
+export const favoriteBlog = (bloglist) => {
+	let favoriteBlog = bloglist.length > 0 ? bloglist[0] : null
+
+	bloglist.forEach(blog => {
+		if (blog.likes > favoriteBlog.likes) {
+			favoriteBlog = blog
+		}
+	})
+
+	return favoriteBlog
+}
