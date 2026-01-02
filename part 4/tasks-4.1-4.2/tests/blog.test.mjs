4f350db912c2547c3f42cf8eae5ae38950f4c09c
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-03 01:01:28 +0200

    chore: task 4.4: Helper Functions and Unit Tests, step 2

--- part 4/tasks-4.1-4.2/tests/blog.test.mjs
+++ part 4/tasks-4.1-4.2/tests/blog.test.mjs
@@ -1,10 +1,30 @@
-import {test} from 'node:test'
+import {test, describe} from 'node:test'
 import assert from 'assert'
-import {dummy}  from "../utils/blogListHelper.mjs";
+import {dummy, totalLikes } from "../utils/blogListHelper.mjs";
 
-test('should return 1', () => {
-	const blogList = []
+describe('blog list', () => {
+	test('should return 1', () => {
+		const blogList = []
 
-	const result = dummy(blogList)
-	assert.strictEqual(result, 1)
+		const result = dummy(blogList)
+		assert.strictEqual(result, 1)
+	})
+})
+
+describe('total likes per blog post', () => {
+	const listWithOneBlog = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+			likes: 5,
+			__v: 0
+		}
+	]
+
+	test('should return 5', () => {
+		const result = totalLikes(listWithOneBlog)
+		assert.strictEqual(result, 5)
+	})
 })
--- part 4/tasks-4.1-4.2/utils/blogListHelper.mjs
+++ part 4/tasks-4.1-4.2/utils/blogListHelper.mjs
@@ -1,3 +1,7 @@
 export const dummy = (bloglist) => {
 	return 1
 }
+
+export const totalLikes = (bloglist) => {
+	return bloglist.reduce((likes, blog) => likes + blog.likes, 0)
+}
