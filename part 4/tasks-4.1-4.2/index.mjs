e5258fd7faa8c7ffc6f1614bcd848a5ef40866d8
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-02 09:08:34 +0200

    chore: task 4.1 Blog List, step 1

---
+++ part 4/tasks-4.1-4.2/index.mjs
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(process.env.MONGOURL, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+	Blog.find({}).then((blogs) => {
+		response.json(blogs)
+	})
+})
+
+app.post('/api/blogs', (request, response) => {
+	const blog = new Blog(request.body)
+
+	blog.save().then((result) => {
+		response.status(201).json(result)
+	})
+})
+
+const PORT = 3003
+app.listen(process.env.PORT || 3003, () => {
+	console.log(`Server running on port ${process.env.PORT}`)
+})
