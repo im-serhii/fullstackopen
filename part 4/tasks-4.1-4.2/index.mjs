59bdf3f32669d414b5a0e1d334913d5971f66661
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-04 07:18:51 +0200

    fix: small fixes and changes

--- part 4/tasks-4.1-4.2/index.mjs
+++ part 4/tasks-4.1-4.2/index.mjs
@@ -1,7 +1,13 @@
-import {info} from "./utils/logger.mjs";
+import {error, info} from "./utils/logger.mjs";
 import {app} from "./app.mjs";
-import {port} from "./utils/config.mjs";
+import {dbUrl, port} from "./utils/config.mjs";
+import mongoose from "mongoose";
 
 app.listen(port, () => {
 	info(`Server running on port ${port}`)
+
+	info('connecting to the database')
+	mongoose.connect(dbUrl, { family: 4 })
+		.then(() => info('Connected to the database'))
+		.catch((err) => error(err.message));
 })
--- part 4/tasks-4.1-4.2/utils/timeStamp.mjs
+++ part 4/tasks-4.1-4.2/utils/timeStamp.mjs
@@ -6,5 +6,5 @@ export const timeStamp = () => {
 	const dayName = now.toLocaleString('en-US', {weekday: 'long',})
 	const monthName = now.toLocaleString('en-US', {month: 'long',})
 
-	return `${now.hour}:${now.minute}:${now.second} | ${now.day} ${dayName} ${monthName} ${now.year}`
+	return `${now.hour}:${now.minute}:${now.second} | ${dayName} ${now.day} ${monthName} ${now.year}`
 }
