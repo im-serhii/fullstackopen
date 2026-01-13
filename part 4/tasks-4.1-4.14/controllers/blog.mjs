10e99f5222091e6bcb36bdd1440f078b0c6f5f7c
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-13 09:14:23 +0200

    chore: 4.17: Blog List Expansion, step 5

--- part 4/tasks-4.1-4.14/controllers/blog.mjs
+++ part 4/tasks-4.1-4.14/controllers/blog.mjs
@@ -1,38 +1,53 @@
 import express from "express";
 import Blog from "../models/blog.mjs";
-import {Error} from "../utils/logger.mjs";
-import mongoose from "mongoose";
-
+import User from "../models/user.mjs";
 export const blogRouter = express.Router();
 
-blogRouter.get('/', async(request, response) => {
-	const blogs = await Blog.find({})
-	response.json(blogs)
-})
-
-blogRouter.post('/', async (request, response) => {
-	const blog = new Blog(request.body);
-
+blogRouter.get('/', async(request, response, next) => {
 	try {
-		const addedBlog = await blog.save()
-		response.status(201).json(addedBlog).end()
+		const blogs = await Blog.find({}).populate('user', {name: 1, username: 1})
+		response.json(blogs)
 	} catch (err) {
-		error(err.message)
-		response.status(400).json({error: err.name})
+		next(err)
 	}
 })
 
-blogRouter.delete('/:id', async (request, response) => {
+blogRouter.post('/', async (request, response, next) => {
+	const body = request.body
+	const user = await User.findOne({})
+
+	if (!user) {
+		return response.status(400).json({error: "User does not exist"})
+	}
+
+	const blog = new Blog({
+		user: user.id,
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes,
+	})
+
+	try {
+		const addedBlog = await blog.save()
+		user.blogs = user.blogs.concat(addedBlog._id)
+		await user.save()
+		response.status(201).json(addedBlog)
+	} catch (err) {
+		next(err)
+	}
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
 	try {
 		await Blog.findByIdAndDelete(request.params.id)
 		response.status(204).end()
 	} catch (err) {
-		Error(err.message)
-		response.status(400).json({error: err.name})
+		next(err)
 	}
 })
 
-blogRouter.put('/:id', async (request, response) => {
+blogRouter.put('/:id', async (request, response, next) => {
 	try {
 		const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, {
 			new: true,
@@ -47,7 +62,6 @@ blogRouter.put('/:id', async (request, response) => {
 
 
 	} catch (err) {
-		error(err.message)
-		response.status(400).json({error: err.name})
+		next(err)
 	}
 })
