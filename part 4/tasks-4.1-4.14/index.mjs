1401296e50990432ff969174eaf786affe29ace6
Serhii Didenko <serhii.didenko@icloud.com>
2026-01-23 08:36:42 +0200

    fix: fix upper task dir numbers

--- part 4/tasks-4.1-4.14/index.mjs
+++
@@ -1,7 +0,0 @@
-import {Info} from "./utils/logger.mjs";
-import {app} from "./app.mjs";
-import {port} from "./utils/config.mjs";
-
-app.listen(port, () => {
-	Info(`Server running on port ${port}`)
-})
--- part 4/tasks-4.1-4.14/utils/config.mjs
+++
@@ -1,5 +0,0 @@
-export const port = process.env.PORT || 3003
-export const secret = process.env.SECRET || 'secret'
-export const dbUrl = process.env.NODE_ENV === 'test'
-	? process.env.MONGOURL_TEST_ENV
-	: process.env.MONGOURL
